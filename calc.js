1c5f7f0f490e069cc3a96a4099932a69f5c7f2c8
ritamargo25 <daisypearl@mail.ru>
2022-11-06 23:56:06 +0300

    init commit

---
+++ calc.js
@@ -0,0 +1,30 @@
+function sum(a, b) {
+    let a = prompt('Введи первое число');
+    let b = prompt('Введи второе число');
+    let result = (a + b);
+    alert(result);
+}
+
+
+function minus(a, b) {
+    let a = prompt('Введи первое число');
+    let b = prompt('Введи второе число');
+    let result = (a - b);
+    alert(result);
+}
+
+function multiply(a, b) {
+    let a = prompt('Введи первое число');
+    let b = prompt('Введи второе число');
+    let result = (a * b);
+    alert(result);
+}
+
+function divide(a, b) {
+    let a = prompt('Введи первое число');
+    let b = prompt('Введи второе число');
+    let result = (a / b);
+    alert(result);
+}
+
+
---
+++ index.js
@@ -0,0 +1,6 @@
+function hello() {
+    let person = prompt('Как тебя зовут?');
+    alert(`Привет, ${person}! А я - Рита!`);
+}
+
+
